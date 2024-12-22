f5bc406e3cb04857efa158134070ed2e200a0155
hamed moradi <99538932+hameddjf@users.noreply.github.com>
2024-12-22 23:13:36 +0330

    Update settings.py

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'online_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
